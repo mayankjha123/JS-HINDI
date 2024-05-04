53850dbd22d79ee9235b4bbc9844ed3aaf0a7ff2
mjzzz <107613106+mayankjha123@users.noreply.github.com>
2024-05-04 18:07:51 +0000

    modified:   01_basics/03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,5 +21,30 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 
 let someNumber= 33
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+// *******************operations***********************
+
+let value=3
+let negValue=-value;
+
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1="hello"
+let str2=" mayank"
+
+let str3=str1 + str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log( "1" + "2");
+console.log("1"+2+2)
