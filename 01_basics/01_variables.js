11b39854b5bc12ce7b0ea6e08401f53f31ddbdbe
mjzzz <107613106+mayankjha123@users.noreply.github.com>
2024-05-04 07:50:42 +0000

    new file:   01_basics/01_variables.js     new file:   01_basics/02_datatypes.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=14453
+let accountMail="mayank@gmail.com"
+var accountPasssword="12345"
+accountCity="jaipur"
+let accountState
+
+//accountId=2 not allowed
+
+
+
+accountEmail="hsbc2@gmail.com"
+accountPasssword="2121212"
+accountCity="delhi"
+
+/*
+prefer not use var
+bcoz of issue in block space and functional scope
+*/
+console.log(accountId);
+console.table([accountId,accountMail,accountPasssword,accountCity,accountState ])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) we are using Nodejs, not browser
+
+console.log(3+3) 
+
+console.log("mayank")
+
+let name="mayank"
+let age=23
+let isLoggedIn= false
+let state=null
+
+//number=>2 to power 53
+//bigint
+//string=""
+//boolean=true/false
+//null=> stabdalone value
+//undefined
+
+console.log(typeof "hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
