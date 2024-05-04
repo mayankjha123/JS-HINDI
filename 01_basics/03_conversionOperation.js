e4d8a2cc7f643503f65926f8f163061a45bf68ad
mjzzz <107613106+mayankjha123@users.noreply.github.com>
2024-05-04 15:43:30 +0000

    new file:   01_basics/03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score= '33'
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// '33'=33
+// "33abc"=NaN
+// true=1; false=0
+
+let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=true;0=false
+// ""=false;"mayank"=true;
+
+let someNumber= 33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
