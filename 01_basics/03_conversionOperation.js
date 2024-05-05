568778d821dea0a469a0cfe0e075af3a59fe4fa2
mjzzz <107613106+mayankjha123@users.noreply.github.com>
2024-05-05 14:09:47 +0000

    modified:   01_basics/03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -43,8 +43,13 @@ let str1="hello"
 let str2=" mayank"
 
 let str3=str1 + str2
-console.log(str3);
-console.log("1"+2);
-console.log(1+"2");
-console.log( "1" + "2");
-console.log("1"+2+2)
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log( "1" + "2");
+// console.log("1"+2+2)
+
+// console.log(3+4*8%5);
+
+//console.log(+true);
+
