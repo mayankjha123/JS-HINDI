4a2a5d89818193816dc6042f74b900be6f501a1f
mjzzz <107613106+mayankjha123@users.noreply.github.com>
2024-05-18 11:42:26 +0000

    new file:   01_basics/05_datatypes-summary.js

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,41 @@
+//primitive 
+
+// 7 types : string , number,  boolean , null , undefined , symbol, BigINt
+
+const score = 100
+const scoreId=false
+
+const id=Symbol("123")
+const Anotherid=Symbol("123")
+console.log(id==Anotherid)
+
+//refrence (non primitive)
+
+//array. , objects, functions
+
+const arr=[10,2,3,4,5]
+let myObj={
+    name: "mayank",
+    age:22
+}
+
+const myFunction=function()
+{
+    console.log("hello world");
+}
+
+
+console.log(typeof myObj);
+
+
+//++++++++++++++++memory
+
+//stack(primitive), heap(non-primitive)
+
+ let myName="mayank"
+
+ let anothername=myName
+ anothername="sam"
+
+ console.log(myName)
+ console.log(anothername)
